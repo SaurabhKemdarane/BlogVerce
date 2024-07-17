17b0fa251768560c413c2de55f95b93bd7c5f31a
saurabh kemdarane <127221775+SaurabhKemdarane@users.noreply.github.com>
2024-07-18 03:16:24 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
-import logo from './Assets/logo.png';
+import logo from './Assets/LOGO.png';
+
+function Logo({ width = '100px', height = 'auto' }) {
+  return (
+    <div>
+      <img src={logo} alt="BlogVerce" style={{ width: width, height: height }} />
+    </div>
+  );
+}
+
+export default Logo;
+
 
 function Logo({ width = '100px', height = 'auto' }) {
   return (
